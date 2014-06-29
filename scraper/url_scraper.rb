2439e61d7aa1fa1225a427c4a7dea2d2a53c7f32
Gabe Polk <gabe.a.polk@gmail.com>
2014-06-29 15:54:35 -0700

    Successfully implemented url scrape for array of URLs

--- scraper/url_scraper.rb
+++ scraper/url_scraper.rb
@@ -1,45 +1,26 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry-debugger'
+require 'json'
 
 speech_urls = 'html/get_urls.html'
 doc1 = Nokogiri::HTML(open(speech_urls))
 
-# speeches = doc1.css('#listing')
-
-# speeches.each do |speech|
-#   # binding.pry
-#   puts speech.css('.president').text.strip
-#   puts speech.css('.title').text.strip
-# end
-
 # gets all urls for speeches
 speech_url_arr = doc1.css('#listing .title a').map { |link| link['href'] }
-puts speech_url_arr
+puts speech_url_arr.count
 
-# 1.times do |speech|
-#   speech.css('.')
-# end
+speech_url_arr.each_with_index do |speech, i|
+  speech_transcript = speech
+  doc2 = Nokogiri::HTML(open(speech_transcript))
 
-speech_transcript = 'html/get_speech.html'
-doc2 = Nokogiri::HTML(open(speech_transcript))
+  president = doc2.at_css('#innercontent h2').text.strip
+  speech = doc2.at_css('#innercontent h1').text.strip.split(" (").first
+  transcript = doc2.at_css('#innercontent #transcript').text.strip
+  date = doc2.at_css('#innercontent h1').text.strip.split("(").last[0...-1]
 
-# binding.pry
-speech = doc2.at_css('#innercontent h1').text.strip
-president = doc2.at_css('#innercontent h2').text.strip
-transcript = doc2.at_css('#innercontent #transcript').text.strip
-
-File.open("output/#{president.gsub(/\s+/, "").downcase} - #{speech.downcase}.txt", "w") do |f|
-  f.write("#{president}\n#{transcript}")
+  # create .txt file with correct title
+  File.open("output/#{president.gsub(/[\s+\/-]/, "_").downcase + i.to_s}.txt", "w") do |f|
+    f.write("{title} #{speech} {/title}\n{speaker} #{president} {/speaker}\n{date} #{date} {/date}\n\n{speech}\n\n#{transcript}\n\n{/speech}")
+  end
 end
-
-
-# speech_url_arr.each do |speech|
-#   puts doc2.css('#innercontent')
-# end
-
-
-
-
-
-
