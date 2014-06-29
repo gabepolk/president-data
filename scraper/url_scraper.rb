d57449ebca5ffb64b7f53340087c06c280e2d39a
Gabe Polk <gabe.a.polk@gmail.com>
2014-06-29 16:02:11 -0700

    Added comments, cleaned up code

--- scraper/url_scraper.rb
+++ scraper/url_scraper.rb
@@ -1,7 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry-debugger'
-require 'json'
 
 speech_urls = 'html/get_urls.html'
 doc1 = Nokogiri::HTML(open(speech_urls))
@@ -10,16 +9,18 @@ doc1 = Nokogiri::HTML(open(speech_urls))
 speech_url_arr = doc1.css('#listing .title a').map { |link| link['href'] }
 puts speech_url_arr.count
 
+# loop through array of speech urls and scrape
 speech_url_arr.each_with_index do |speech, i|
   speech_transcript = speech
   doc2 = Nokogiri::HTML(open(speech_transcript))
 
+  # assign speech attributes
   president = doc2.at_css('#innercontent h2').text.strip
   speech = doc2.at_css('#innercontent h1').text.strip.split(" (").first
   transcript = doc2.at_css('#innercontent #transcript').text.strip
   date = doc2.at_css('#innercontent h1').text.strip.split("(").last[0...-1]
 
-  # create .txt file with correct title
+  # create .txt file with correct title + metatags
   File.open("output/#{president.gsub(/[\s+\/-]/, "_").downcase + i.to_s}.txt", "w") do |f|
     f.write("{title} #{speech} {/title}\n{speaker} #{president} {/speaker}\n{date} #{date} {/date}\n\n{speech}\n\n#{transcript}\n\n{/speech}")
   end
