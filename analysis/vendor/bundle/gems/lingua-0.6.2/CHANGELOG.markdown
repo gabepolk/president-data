4417589016163f459af22115c79684445573f62c
andrewpei <andrewpei@gmail.com>
2014-07-02 09:54:11 -0700

    Clean directory to begin doing server work, organized correctly I believe

--- analysis/vendor/bundle/gems/lingua-0.6.2/CHANGELOG.markdown
+++
@@ -1,3 +0,0 @@
-0.6.2
------
-* Fix for sentences that end in abbreviations (e.g. dr, mrs, ms) [chad]
--- analysis/vendor/bundle/gems/lingua-0.6.2/lib/lingua.rb
+++
@@ -1,6 +0,0 @@
-prefix = File.dirname(__FILE__) + "/"
-$LOAD_PATH.unshift prefix
-
-Dir.glob(prefix + "**/*.rb").each do |f|
-  require File.expand_path(f)
-end
